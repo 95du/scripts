d10c5b6a2b75fe90755e95b6bdc8fc7faeb1f4b3
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-10-03 02:48:58 +0800

    Add files via upload

---
+++ rewrite/get_laohu8_Authorization.conf.js
@@ -0,0 +1,6 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-green; icon-glyph: magic;
+# 获取市值股票 Authorization
+^https:\/\/hq\.laohu8\.com url script-request-header https://raw.githubusercontent.com/95du/scripts/main/rewrite/get_laohu8_Authorization.js
+hostname = hq.laohu8.com
