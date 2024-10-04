3999859b4c6c0f3b1ba97d8cc6e3dd257a868509
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-10-04 20:46:44 +0800

    Add files via upload

---
+++ rewrite/get_amap_family_sport.conf.js
@@ -0,0 +1,8 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-green; icon-glyph: magic;
+# 获取高德家人地图运动 url
+
+^https:\/\/m5\.amap\.com\/ws\/mapapi\/sport\/family url script-request-header https://raw.githubusercontent.com/95du/scripts/main/rewrite/get_amap_family_sport.js
+
+hostname = m5.amap.com
