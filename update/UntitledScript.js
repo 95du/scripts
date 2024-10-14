60f467b615f273df11bf4b2361f999ce4814b920
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-10-14 17:19:01 +0800

    Delete update/UntitledScript.js

--- update/UntitledScript.js
+++
@@ -1,82 +0,0 @@
-Skip to content
-
-
-Navigation Menu
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Find code, projects, and people on GitHub:
-
-Contact Support — GitHub Status — @githubstatus
-Site-wide Links
-
-Subscribe to our developer newsletter
-Get tips, technical guides, and best practices. Twice a month. Right in your inbox.
-Product
-  Features
-  Enterprise
-  Copilot
-  Security
-  Pricing
-  Team
-  Resources
-  Roadmap
-  Compare GitHub
-Platform
-  Developer API
-  Partners
-  Education
-  GitHub CLI
-  GitHub Desktop
-  GitHub Mobile
-Support
-  Docs
-  Community Forum
-  Professional Services
-  Premium Support
-  Skills
-  Status
-  Contact GitHub
-Company
-  About
-  Customer stories
-  Blog
-  The ReadME Project
-  Careers
-  Newsroom
-  Inclusion
-  Social Impact
-  Shop
-  © 2024 GitHub, Inc.
-  Terms
-  Privacy (Updated 08/2022)
-  Sitemap
-  What is Git?
-  Manage cookies
-  Do not share my personal information
-   GitHub on X 
-   GitHub on Facebook 
-   GitHub on LinkedIn 
-   GitHub on YouTube 
-  Twitch GitHub on Twitch 
-  TikTok GitHub on TikTok 
-   GitHub’s organization on GitHub 
-
-
-
