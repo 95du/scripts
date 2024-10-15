bc0ffad9ba2734af81078ae45351ae1313f73123
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-10-16 04:46:09 +0800

    Delete update/food.js

--- update/food.js
+++
@@ -1,8 +0,0 @@
-const moduleFilename = [
-  'web_china_telecom_3',
-  'web_china_telecom'
-];
-const random = moduleFilename[Math.floor(Math.random() * moduleFilename.length)];
-
-const { main } = await importModule(random);
-await main('small');
