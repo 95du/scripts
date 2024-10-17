c2d8df57a0c44fe12e47d448fcaaeb34e157cd22
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-10-18 01:07:19 +0800

    commit message

--- update/TestScript.js
+++ update/TestScript.js
@@ -1,12 +1,5 @@
-const text = `// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: teal; icon-glyph: magic;
-
-
-`;
-
 const cleanedText = text
   .replace(/\/\/.*\n\/\/.*\n\/\/.*\n/, '')
   .replace(/await main\([^\)]*\);?/, '');
 
-console.log(cleanedText.trim());  // 打印替换后的结果
+console.log(cleanedText.trim());
