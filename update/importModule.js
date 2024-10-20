1a1641482623505c7ec72ab82c28a439531917bd
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-10-20 16:20:37 +0000

    Update update/importModule.js

--- update/importModule.js
+++
@@ -1,7 +0,0 @@
-const moduleFilename = [
-  'web_china_telecom_3',
-  'web_china_telecom'
-];
-const random = moduleFilename[Math.floor(Math.random() * moduleFilename.length)];
-const { main } = await importModule(random);
-温柔以待
