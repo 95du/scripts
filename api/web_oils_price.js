0335f31f5007511f57f1f8f5c43a13506942bf55
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-10-22 02:31:53 +0800

    Update File:  api/web_oils_price.js

--- api/web_oils_price.js
+++ api/web_oils_price.js
@@ -12,6 +12,7 @@
 async function main() {
   const fm = FileManager.local();
   const mainPath = fm.joinPath(fm.documentsDirectory(), "95du_Oils");
+  const rootUrl = 'https://raw.githubusercontent.com/95du/scripts/master';
   
   const getCachePath = (dirName) => fm.joinPath(mainPath, dirName);
   const [ settingPath, cacheImg, cacheStr ] = [
@@ -71,12 +72,12 @@ async function main() {
         const path = fm.joinPath(basePath, name);
         if (fm.fileExists(path)) {
           if (hasExpired(path) > cacheTime || province !== setting.oils[0]) fm.remove(path);
-          else return type == 1 ? JSON.parse(fm.readString(path)) : type == 2 ? fm.readString(path) : fm.readImage(path);
+          else return type ? JSON.parse(fm.readString(path)) : fm.readImage(path);
         }
       },
       write: (name, content) => {
         const path = fm.joinPath(basePath, name);
-        type == 1 ? fm.writeString(path, JSON.stringify(content)) : type == 2 ? fm.writeString(path, content) : fm.writeImage(path, content);
+        type ? fm.writeString(path, JSON.stringify(content)) : fm.writeImage(path, content);
       }
     };
   
@@ -94,11 +95,11 @@ async function main() {
    */
   const getCacheData = async (name, url, type) => {
     const cache = useFileManager({  
-      cacheTime: type ? 4 : 24, type
+      cacheTime: type ? 4 : 240, type
     });
     const cacheData = cache.read(name);
     if (cacheData) return cacheData;
-    const response = await new Request(url)[type == 1 ? 'loadJSON' : type == 2 ? 'loadString' : 'loadImage']();
+    const response = await new Request(url)[type ? 'loadJSON' : 'loadImage']();
     if (response) {
       cache.write(name, response);
     }
@@ -119,11 +120,10 @@ async function main() {
   };
     
   // 初始化，预警通知
-  const updateAndNotify = (oils, oilsAlert, oilsTips) => {
-    if (setting.oilsAlert !== oilsAlert || setting.oilsTips !== oilsTips || province !== setting.oils[0]) {
-      if (setting.oilsAlert !== oilsAlert) notify(`${province}油价调整‼️`, oilsAlert);
-      if (setting.oilsTips !== oilsTips) notify(`${province}油价调整‼️`, oilsTips);
-      Object.assign(setting, { oilsAlert, oilsTips, oils });
+  const updateAndNotify = (oils, oilsTips) => {
+    if (setting.oilsTips !== oilsTips || province !== setting.oils[0]) {
+      notify(`${province}油价调整‼️`, oilsTips);
+      Object.assign(setting, { oilsTips, oils });
       writeSettings(setting);
     }
   };
@@ -178,52 +178,21 @@ async function main() {
     { title: '新疆维吾尔自治区' }
   ];
   
-  const findTitle = (province) => {
+  const findTitle = () => {
     const item = provinces.find(item => item.title.includes(province));
-    return item ? item.title : '海南';
+    return item ? item.title : '海南';  
   };
   
-  const getOilTips = async () => {
-    const url = 'https://20121212.cn/ci/index.php/tips/get';  
-    const [data] = await getCacheData('tips.json', url, 1);
-    return data ? data.tips : null;
-  };
-  
-  const getOilsPrices = async (province) => {
+  const getOilsPrices = async () => {
+    const province = findTitle();
     const url = `https://youjia.15qs.com/index.php?c=api&a=getprice&province=${encodeURIComponent(province)}`;  
-    const oil = await getCacheData('oil.json', url, 1);
+    const oil = await getCacheData('oil.json', url, true);
     return oil ? [setting.province, oil.hao92, oil.hao95, oil.hao98, oil.hao0] : null;
   };
   
-  const getOilsData = async () => {
-    try {
-      const html = await getCacheData('oil.html', 'http://m.qiyoujiage.com', 2);
-      const webView = new WebView();
-      await webView.loadHTML(html);
-      
-      const { tishiContent, prices } = await webView.evaluateJavaScript(`
-        (() => {
-          const row = [...document.querySelectorAll('table tr')].find(tr => tr.querySelector('td').textContent.trim() === "${setting.province}");
-          const prices = row ? [...row.querySelectorAll('td')].map(td => td.textContent.trim()) : null;
-          return { prices, tishiContent };
-        })();
-      `);
-      
-      const match = [province, ...html.match(new RegExp(`${province}<\\/a><\\/td><td>(\\d+\\.\\d+)<\\/td><td>(\\d+\\.\\d+)<\\/td><td>(\\d+\\.\\d+)<\\/td><td>(\\d+\\.\\d+)`))?.slice(1)];
-      
-      const title = findTitle(province);
-      const oils = prices || await getOilsPrices(title) || match;
-      const oilsAlert = tishiContent.replace('<br/>', '，');
-      return { oilsAlert, oils };
-    } catch (e) {
-      console.log(`${e}\n使用缓存`);
-    }
-  };
-  
-  const oilsTips = await getOilTips() || setting.oilsTips;
-  const { oilsAlert, oils } = await getOilsData() || setting;
-  const [_, oil92, oil95, oil98, oil0] = oils.map(item => parseFloat(item).toPrecision(3));
-
+  const oils = await getOilsPrices() || setting.oils;
+  const [_, oil92, oil95, oil98, oil0] = oils.map(item => parseFloat(item).toPrecision(3));  
+  // 生成油品类型数组
   const oilTypes = [
     { name: '0#', value: oil0,  color: '#FB8C00' },
     { name: '92', value: oil92, color: '#3F8BFF' },
@@ -231,28 +200,26 @@ async function main() {
     { name: '98', value: oil98, color: '#BE38F3' },
   ];
   
-  // 计算时间剩余天数
-    const calculateDaysDiff = (targetDateString) => {
-    const currentDate = new Date();
-    const currentYear = currentDate.getFullYear();
-    const dateString = `${currentYear}年${targetDateString}`;
-    const [year, month, day] = dateString.match(/\d+/g);
-    const targetDate = new Date(year, month - 1, day, 24, 0, 0);
-    const timeDifference = targetDate - currentDate;
-    return Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+  // 获取油价预警
+  const getOilTips = async () => {
+    const url = 'https://20121212.cn/ci/index.php/tips/get';  
+    const [data] = await getCacheData('tips.json', url, true);
+    
+    const cleanText = (text) => text.replace(/\s+/g, ' ');
+    const match = data.tips.match(/^\d{1,2}月\d{1,2}日/);
+    const tips = data.tips.match(/(\d{1,2}月\d{1,2}日，|预测).*/);
+    return {
+      date: Math.floor((new Date(data.timedown) - new Date()) / 86400000),
+      oilsTips: cleanText(!match && tips ? tips[0] : data.tips)
+    }
   };
   
-  const addAlert = (() => {
-    const date = oilsAlert.match(/(\d{1,2}月\d{1,2}日)/)[0];
-    const daysDiff = date ? calculateDaysDiff(date) : 0;
-    const surplus = daysDiff <= 0 ? '全国' : `【 剩余 ${daysDiff} 天 】 `;
-    const alert = oilsAlert.length < 48 ? `${oilsAlert}涨价几毛叫“微调”，降价几分叫“跌破”。` : oilsAlert;
-    return surplus + alert;
-  })();
+  const { oilsTips, date } = await getOilTips() || setting;
+  const tipsGap = oilsTips && oilsTips.length >= 78;
   
   // 设置组件背景
   const setBackground = async (widget) => {
-    const backgroundImage = await getCacheData('logo.png', 'https://gitcode.net/4qiao/scriptable/raw/master/img/jingdong/baiTiaoBg.png');
+    const backgroundImage = await getCacheData('logo.png', `${rootUrl}/img/background/glass_0.png`);
     const bgImage = getBgImage();
     if (fm.fileExists(bgImage) && !isDark) {
       widget.backgroundImage = await shadowImage(fm.readImage(bgImage));
@@ -282,9 +249,6 @@ async function main() {
   
   // createWidget
   const createWidget = async () => {
-    const tips = getRandomItem([oilsTips, addAlert]);
-    const tipsGap = tips === oilsTips && oilsTips.length >= 80;
-    
     const widget = new ListWidget();
     widget.setPadding(10, 10, 10, 10)
     const mainStack = widget.addStack();
@@ -326,7 +290,7 @@ async function main() {
     columnStack.backgroundColor = Color.red();
     statusStack.addSpacer();
     
-    const oilTipsText = statusStack.addText(tips);
+    const oilTipsText = statusStack.addText(oilsTips + (oilsTips.length >= 75 || date < 1 ? '' : ` 【 剩余 ${date} 天 】`));
     oilTipsText.textColor = textColor
     oilTipsText.font = Font.mediumSystemFont(tipsGap ? font : 14);
     oilTipsText.leftAlignText();
@@ -369,7 +333,7 @@ async function main() {
     const family = config.widgetFamily || 'medium';
     const widget = await (family === 'medium' ? createWidget() : createErrorWidget());
     await setBackground(widget);
-    updateAndNotify(oils, oilsAlert, oilsTips);
+    updateAndNotify(oils, oilsTips);
     
     if (config.runsInApp) {
       await widget[`present${family.charAt(0).toUpperCase() + family.slice(1)}`]();
