9cd5f82634528528e51510f46eb0fb31f970a52a
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-10-24 20:47:46 +0800

    Update File:  widget/congest_4.js

--- widget/congest_4.js
+++ widget/congest_4.js
@@ -40,7 +40,7 @@ const { name, label, typeName, type } = await getSettings(cacheFile);
 
 //
 const autoUpdate = async () => {
-  const script = await new Request('https://gitcode.net/4qiao/scriptable/raw/master/api/congest_4.js').loadString();
+  const script = await new Request('https://raw.githubusercontent.com/95du/scripts/master/widget/congest_4.js').loadString();
   fm.writeString(module.filename, script);
 };
 
