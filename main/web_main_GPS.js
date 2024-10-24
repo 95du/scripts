e83fe45c17c8da27443fcd1fd92639afe63ce1a4
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-10-24 12:16:39 +0800

    Update File:  main/web_main_GPS.js

--- main/web_main_GPS.js
+++ main/web_main_GPS.js
@@ -2,18 +2,17 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: teal; icon-glyph: car;
 
-
 async function main() {
   const scriptName = 'GPS 定位器'
-  const version = '1.0.0'
+  const version = '1.1.0'
   const updateDate = '2023年08月12日'
   
   const pathName = '95du_GPS';
+  const rootUrl = 'https://raw.githubusercontent.com/95du/scripts/master';
+  
+  const [scrName, scrUrl] = ['gps_locating.js', `${rootUrl}/api/web_gps_locating.js`];
+
   const widgetMessage = '功能: 通过GPS设备制作的中小号小组件，显示车辆实时位置、车速、最高时速、行车里程和停车时间等。推送实时静态地图及信息到微信。需申请高德地图 web 服务 Api 类型 key，微信推送需要另外填入企业微信应用的Api信息。';
-  
-  const rootUrl = atob('aHR0cHM6Ly9naXRjb2RlLm5ldC80cWlhby9mcmFtZXdvcmsvcmF3L21hc3Rlci8=');
-  
-  const [scrName, scrUrl] = ['gps_locating.js', 'https://gitcode.net/4qiao/scriptable/raw/master/table/web_gps_locating.js'];
 
   /**
    * 创建，获取存储路径
@@ -127,10 +126,10 @@ async function main() {
    * @param {string} url
    * @param {string} sound
    */
-  const notify = async (title, body, url, opts = {}) => {
-    const n = Object.assign(new Notification(), { title, body, sound: 'piano_', ...opts });
+  const notify = (title, body, url, opts = {}) => {
+    const n = Object.assign(new Notification(), { title, body, sound: 'event', ...opts });
     if (url) n.openURL = url;
-    return await n.schedule();
+    n.schedule();
   };
   
   /**
@@ -167,7 +166,7 @@ async function main() {
   
   /** download store **/
   const myStore = async () => {
-    const script = await getString('https://gitcode.net/4qiao/scriptable/raw/master/api/95duScriptStore.js');
+    const script = await getString(`${rootUrl}/run/web_module_95duScript.js`);
     const fm = FileManager.iCloud();
     fm.writeString(
       fm.documentsDirectory() + '/95du_ScriptStore.js', script);
@@ -274,7 +273,9 @@ async function main() {
     const cssString = cache.readString(cssFileName);
     if (cssString) return cssString;
     const response = await getString(cssFileUrl);
-    cache.writeString(cssFileName, response);
+    if (!response.includes('!DOCTYPE')) {  
+      cache.writeString(cssFileName, response);
+    }
     return response;
   };
   
@@ -514,20 +515,20 @@ async function main() {
       previewImage
     } = options;
     
-    const appleHub_light = await getCacheImage('white.png', `${rootUrl}img/picture/appleHub_white.png`);
-    const appleHub_dark = await getCacheImage('black.png', `${rootUrl}img/picture/appleHub_black.png`);
+    const appleHub_light = await getCacheImage('white.png', `${rootUrl}/img/picture/appleHub_white.png`);
+    const appleHub_dark = await getCacheImage('black.png', `${rootUrl}/img/picture/appleHub_black.png`);
     
-    const appImage = await getCacheImage('aMapAppImage.png', `${rootUrl}img/icon/aMap.png`);
+    const appImage = await getCacheImage('aMapAppImage.png', `${rootUrl}/img/icon/aMap.png`);
     
-    const collectionCode = await getCacheImage('collection.png', `${rootUrl}img/picture/collectionCode.jpeg`);
+    const collectionCode = await getCacheImage('collection.png', `${rootUrl}/img/picture/collectionCode.jpeg`);
     
-    const authorAvatar = fm.fileExists(getAvatarImg()) ? await toBase64(fm.readImage(getAvatarImg()) ) : await getCacheImage('author.png', `${rootUrl}img/icon/4qiao.png`);
+    const authorAvatar = fm.fileExists(getAvatarImg()) ? await toBase64(fm.readImage(getAvatarImg()) ) : await getCacheImage('author.png', `${rootUrl}/img/icon/4qiao.png`);
     
-    const clockScript = await getCacheString('clock.html', `${rootUrl}web/clock.html`);
+    const clockScript = await getCacheString('clock.html', `${rootUrl}/web/clock.html`);
     
     const scripts = ['jquery.min.js', 'bootstrap.min.js', 'loader.js'];
     const scriptTags = await Promise.all(scripts.map(async (script) => {
-      const content = await getCacheString(script, `${rootUrl}web/${script}?ver=7.4.2`);
+      const content = await getCacheString(script, `${rootUrl}/web/${script}%3Fver%3D8.0`);
       return `<script>${content}</script>`;
     }));
     
@@ -558,7 +559,7 @@ async function main() {
      * @param {string} js
      * @returns {string} html
      */
-    const cssStyle = await getCacheString('cssStyle.css', `${rootUrl}web/style.css`);  
+    const cssStyle = await getCacheString('cssStyle.css', `${rootUrl}/web/cssStyle.css`);  
 
     const style =`  
     :root {
@@ -783,8 +784,8 @@ async function main() {
     previewImgHtml = async () => {
       const displayStyle = settings.clock ? 'none' : 'block';
       const previewImgUrl = [
-        `${rootUrl}img/picture/gps_location_0.png`,
-        `${rootUrl}img/picture/gps_location_3.png`
+        `${rootUrl}/img/picture/gps_location_0.png`,
+        `${rootUrl}/img/picture/gps_location_3.png`
       ];
       
       if ( settings.topStyle ) {
@@ -1512,10 +1513,15 @@ async function main() {
           }
           break;
         case 'background':
-          await importModule(await webModule('background.js', 'https://gitcode.net/4qiao/scriptable/raw/master/vip/mainTableBackground.js')).main();
+          const modulePath = webModule('background.js', `${rootUrl}/main/main_background.js`);
+          if (modulePath != null) {
+            await importModule(await modulePath).main(cacheImg);
+            await previewWidget();
+          }
           break;
         case 'store':
-          importModule(await webModule('store.js', 'https://gitcode.net/4qiao/framework/raw/master/mian/module_95du_storeScript.js')).main();
+          const storeModule = webModule('store.js', `${rootUrl}/main/web_main_95du_Store.js`);
+          await importModule(await storeModule).main();
           await myStore();
           break;
         case 'install':
@@ -1624,7 +1630,7 @@ async function main() {
             label: 'AppleOS',
             name: 'appleOS',
             type: 'switch',
-            icon: `${rootUrl}img/symbol/notice.png`
+            icon: `${rootUrl}/img/symbol/notice.png`
           },
           {
             label: '推送时段',
@@ -1647,7 +1653,7 @@ async function main() {
             name: "donate",
             label: "打赏作者",
             type: "cell",
-            icon: 'https://gitcode.net/4qiao/scriptable/raw/master/img/icon/weChat.png'
+            icon: `${rootUrl}/img/icon/weChat.png`
           }
         ]
       }
@@ -1676,7 +1682,7 @@ async function main() {
             name: 'refresh',
             type: 'cell',
             input: true,
-            icon: `${rootUrl}img/symbol/refresh.png`,  
+            icon: `${rootUrl}/img/symbol/refresh.png`,  
             message: '设置桌面组件的时长\n( 单位: 分钟 )',
             desc: settings.refresh
           },
@@ -1689,7 +1695,7 @@ async function main() {
             name: "textLightColor",
             label: "白天文字",
             type: "color",
-            icon: `${rootUrl}img/symbol/title.png`
+            icon: `${rootUrl}/img/symbol/title.png`
           },
           {
             name: "textDarkColor",
@@ -1821,7 +1827,7 @@ async function main() {
             name: 'transparency',
             type: 'cell',
             input: true,
-            icon: `${rootUrl}img/symbol/masking_2.png`,  
+            icon: `${rootUrl}/img/symbol/masking_2.png`,  
             message: '渐变颜色透明度，完全透明设置为 0',
             desc: settings.transparency
           },
@@ -1829,7 +1835,7 @@ async function main() {
             label: '透明背景',
             name: 'background',
             type: 'cell',
-            icon: `${rootUrl}img/symbol/transparent.png`
+            icon: `${rootUrl}/img/symbol/transparent.png`
           },
           {
             label: '遮罩透明',
@@ -1848,14 +1854,14 @@ async function main() {
             name: 'chooseBgImg',
             type: 'file',
             isAdd: true,
-            icon: `${rootUrl}img/symbol/bgImage.png`,
+            icon: `${rootUrl}/img/symbol/bgImage.png`,
             desc: fm.fileExists(getBgImage()) ? '已添加' : ' '
           },
           {
             label: '清除背景',
             name: 'clearBgImg',
             type: 'cell',
-            icon: `${rootUrl}img/symbol/clearBg.png`
+            icon: `${rootUrl}/img/symbol/clearBg.png`
           }
         ]
       },
@@ -1866,7 +1872,7 @@ async function main() {
             label: '布局调整',
             name: 'layout',
             type: 'cell',
-            icon: `${rootUrl}img/symbol/layout.png`,
+            icon: `${rootUrl}/img/symbol/layout.png`,
             message: '建议只调整图片上下空白'
           },
           {
@@ -1916,7 +1922,7 @@ async function main() {
             label: '自动更新',
             name: 'update',
             type: 'switch',
-            icon: `${rootUrl}img/symbol/update.png`
+            icon: `${rootUrl}/img/symbol/update.png`
           },
           {
             label: '背景音乐',
@@ -1945,13 +1951,13 @@ async function main() {
             label: '设置头像',
             name: 'setAvatar',
             type: 'cell',
-            icon: `${rootUrl}img/icon/camera.png`
+            icon: `${rootUrl}/img/icon/camera.png`
           },
           {
             label: 'Telegram',
             name: 'telegram',
             type: 'cell',
-            icon: 'https://gitcode.net/4qiao/scriptable/raw/master/img/icon/NicegramLogo.png'
+            icon: `${rootUrl}/img/icon/Swiftgram.png`
           }
         ]
       },
@@ -2008,7 +2014,7 @@ async function main() {
             label: '重置所有',
             name: 'reset',
             type: 'cell',
-            icon: `${rootUrl}img/symbol/reset.png`
+            icon: `${rootUrl}/img/symbol/reset.png`
           },
           {
             label: '清除缓存',
@@ -2049,7 +2055,7 @@ async function main() {
             label: '预览组件',
             name: 'preview',
             type: 'cell',
-            icon: `${rootUrl}img/symbol/preview.png`
+            icon: `${rootUrl}/img/symbol/preview.png`
           }
         ]
       },
@@ -2070,7 +2076,7 @@ async function main() {
             name: "updateCode",
             label: "更新代码",
             type: "cell",
-            icon: `${rootUrl}img/symbol/update.png`
+            icon: `${rootUrl}/img/symbol/update.png`
           }
         ]
       }
