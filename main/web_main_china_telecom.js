02b59b7ccb56035d3078a7bcd34ebf2f6bbeea01
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-10-25 03:00:26 +0800

    update main/web_main_china_telecom.js

--- main/web_main_china_telecom.js
+++ main/web_main_china_telecom.js
@@ -90,7 +90,7 @@ async function main() {
   const previewWidget = async (family = 'medium') => {
     const moduleJs = await webModule(scrName, scrUrl);
     const { main } = await importModule(moduleJs)
-    
+    await main(family);
     shimoFormData(family);
   };
   
