2adbd93fc1c1dea7c6385905e4e5eccb18a9133b
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-10-25 20:14:31 +0800

    Update File:  run/web_module_china_telecom_3.js

--- run/web_module_china_telecom_3.js
+++ run/web_module_china_telecom_3.js
@@ -60,13 +60,13 @@ const getModuleVersions = () => {
   return [null, null];
 };
 
-const modulePath = await downloadModule();
-if (modulePath) {
-  const importedModule = await importModule(modulePath);
-  try {
+await (async () => {
+  const modulePath = await downloadModule();
+  if (modulePath) {
+    const importedModule = await importModule(modulePath);
     await importedModule.main();
-  } catch(e) {
-    console.log(e)
-    fm.remove(moduleDir);
   }
-};
+})().catch((e) => {
+  console.log(e);
+  fm.remove(moduleDir);
+});
