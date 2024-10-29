8fecf78a118753bdde7b4009edee17ed12e35bb0
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-10-30 00:27:55 +0800

    Update File:  update/GitHub.js

---
+++ update/GitHub.js
@@ -0,0 +1,4 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-gray; icon-glyph: ellipsis-h;
+Safari.openInApp('https://github.com/95du?tab=repositories', false);
