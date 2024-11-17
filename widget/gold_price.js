7f22c80617bf231f02c86c3df03d3f476d5660e2
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-11-17 21:07:49 +0800

    Update File:  widget/gold_price.js

---
+++ widget/gold_price.js
@@ -0,0 +1,185 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-purple; icon-glyph: mercury;
+/**
+ * 组件作者: 95度茅台
+ * 组件名称: 城市通行实况
+ * 组件版本: Version 1.0.0
+ * 发布时间: 2024-03-03 15:30
+ * 组件内容: 全国重点城市 (实时前 10) 拥堵道路排行，早高峰、晚高峰。
+ */
+
+const formatDate = (timestamp, short) => new Date(timestamp + 8 * 3600000).toISOString().slice(short ? 5 : 0, 16).replace('T', ' ');
+
+//
+const autoUpdate = async () => {
+  const script = await new Request('https://raw.githubusercontent.com/95du/scripts/master/widget/gold_price.js').loadString();
+  const fm = FileManager.local();
+  fm.writeString(module.filename, script);
+};
+
+// 获取商店信息
+const getShopInfo = async () => {
+  const url = 'https://m.cngold.org/quote/gjs/swhj.html';
+  const html = await new Request(url).loadString();
+  const webView = new WebView();
+  await webView.loadHTML(html);
+
+  const shopInfo = await webView.evaluateJavaScript(`
+    (() => {
+      const shops = [];
+      const items = document.querySelectorAll('.shop_info_list .shop_info');
+
+      items.forEach(item => {
+        const anchor = item.querySelector('a');
+        const img = item.querySelector('img');
+        const gold = item.querySelector('.gold_price span')?.id.replace(/_price$/, '');
+        const silver = item.querySelector('.silver_price span')?.id.replace(/_price$/, '');
+
+        if (anchor && gold) {
+          shops.push({
+            title: img.getAttribute('title') || img.getAttribute('alt'),
+            url: anchor.getAttribute('href'),
+            img: img.getAttribute('src'),
+            gold,
+            silver
+          });
+        }
+      });
+      return shops;
+    })();
+  `);
+  return shopInfo;
+};
+
+// 获取黄金价格
+const getGoldPrice = async (code) => {
+  const url = `https://api.jijinhao.com/quoteCenter/realTime.htm?codes=${code},&_=${Date.now()}`;
+  
+  try {
+    const req = new Request(url);
+    req.headers = { Referer: 'https://m.cngold.org/' };
+    const data = await req.loadString();
+    const jsonString = data.replace(/^var quote_json =/, '').trim();
+    const jsonData = JSON.parse(jsonString);
+    return jsonData;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// 提取黄金价格数据
+const fetchAndExtract = async () => {
+  const symbolArr = await getShopInfo();
+  
+  const shopInfoArr = await Promise.all(symbolArr.map(async (shop) => {
+    const goldPriceData = await getGoldPrice(shop.gold);
+    const goldPrices = goldPriceData[shop.gold];
+    goldPrices.title = shop.title;
+    return goldPrices;
+  }));
+
+  const filteredShops = shopInfoArr.filter(shop => shop.showCode === 'huangjinjiage');
+  return filteredShops.slice(0, 10);
+};
+
+//
+const getRank = async (stack, { column }) => {
+  let i = -1;
+  const rows = [];
+  const add = async (fn) => {
+    i++;
+    if (i % column === 0) {
+      stack.layoutVertically();
+      rows.push(stack.addStack());
+    }
+    const r = Math.floor(i / column);
+    await fn(rows[r]);
+  };
+  return { add };
+};
+
+const addItem = async (widget, item, max, index) => {
+  const stack = widget.addStack();
+  stack.layoutHorizontally();
+  stack.centerAlignContent();
+  stack.size = new Size(0, 20);
+
+  const indexStack = stack.addStack();
+  indexStack.size = new Size(18, 0);
+  const indexText = indexStack.addText(String(index));
+  indexText.font = Font.boldSystemFont(15);
+  const textColor = index <= 3 
+    ? '#FF0000' : index <= 6
+    ? '#FCA100' : '#00C400';
+  indexText.textColor = new Color(textColor);
+  stack.addSpacer(4);
+  
+  const titleText = stack.addText(item.title);
+  titleText.font = Font.mediumSystemFont(14);
+  titleText.textColor = Color.dynamic(new Color('000000', 0.7), new Color('FFFFFF', 0.9));
+  stack.addSpacer(8);
+  
+  const idxText = stack.addText(String(item.q2));
+  idxText.font = Font.mediumSystemFont(15.5);
+  idxText.textColor = Color.blue();
+  
+  if (item.q2 === max) {
+    stack.addSpacer(10);
+    const barStack = stack.addStack();
+    barStack.size = new Size(10, 10);
+    barStack.cornerRadius = 50;
+    barStack.backgroundColor = Color.red();
+  };
+  stack.addSpacer();
+};
+
+// Create Component Instance
+const createWidget = async () => {
+  const goldArr = await fetchAndExtract();
+  const updateTime = formatDate(goldArr[0].time, true);
+  
+  const widget = new ListWidget();
+  widget.setPadding(15, 15, 15, 15);
+  const mainStack = widget.addStack();
+  mainStack.layoutVertically();
+  mainStack.addSpacer(5);
+  
+  const topStack = mainStack.addStack();
+  topStack.layoutHorizontally();
+  topStack.centerAlignContent();
+  topStack.addSpacer(6.5);
+  
+  const columnStack = topStack.addStack();
+  columnStack.size = new Size(5, 23);
+  columnStack.cornerRadius = 50;
+  columnStack.backgroundColor = new Color('#8B5FF4');
+  topStack.addSpacer(10);
+  
+  const nameText = topStack.addText('黄金时价');
+  nameText.font = Font.boldSystemFont(17);
+  topStack.addSpacer();
+  
+  const dateText = topStack.addText(`更新于 ${updateTime}`);
+  dateText.font = Font.systemFont(14.5);
+  dateText.textOpacity = 0.75;
+  topStack.addSpacer();
+  mainStack.addSpacer();
+  
+  const stackItems = widget.addStack();
+  const { add } = await getRank(stackItems, { column: 2 });
+  const max = Math.max(...goldArr.map(item => item.q2));
+  
+  for (let i = 0; i < 10; ++i) {
+    await add(stack => addItem(stack, goldArr[i], max, i + 1));
+  };
+  
+  if (config.runsInApp) {
+    widget.presentMedium();
+  } else {
+    Script.setWidget(widget);
+    Script.complete();
+  }
+};
+
+await (config.runsInApp || config.widgetFamily === 'medium' ? createWidget() : null);
