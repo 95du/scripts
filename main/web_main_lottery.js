a94e6af0b247ab1ceb54f21b5ec01ec3c473bd13
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-11-22 20:37:49 +0800

    Update File:  main/web_main_lottery.js

--- main/web_main_lottery.js
+++ main/web_main_lottery.js
@@ -191,6 +191,7 @@ async function main() {
     );
     if (index === 0) return;
     await updateString();
+    ScriptableRun();
   };
   
   const updateString = async () => {
@@ -1132,7 +1133,6 @@ async function main() {
           break;
         case 'updateCode':
           await updateVersion();
-          ScriptableRun();
           break;
         case 'period':
           await period(data);
