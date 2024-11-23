c523da99c1921ad3eeef19c07d8ad95be33d7117
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-11-23 13:12:31 +0800

    update main/web_main_lottery.js

--- main/web_main_lottery.js
+++ main/web_main_lottery.js
@@ -779,7 +779,7 @@ async function main() {
         </label>
         <!-- 折叠取色器 -->
         <div class="collapsible-range" id="content">
-          <hr class="range-separ2">
+          <hr class="separ">
           <label class="form-item">
             <div class="form-label">
               <img class="form-label-img" src="\${item.icon}"/>
