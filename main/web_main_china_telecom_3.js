daf38e38c86321178686abe05ff8279021b51fdd
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-11-29 11:29:23 +0800

    Update File:  main/web_main_china_telecom_3.js

--- main/web_main_china_telecom_3.js
+++ main/web_main_china_telecom_3.js
@@ -222,8 +222,6 @@ async function main() {
     const appleHub_light = await module.getCacheImage(`${rootUrl}/img/picture/appleHub_white.png`);
     const appleHub_dark = await module.getCacheImage(`${rootUrl}/img/picture/appleHub_black.png`);
     
-    const appImage = await module.getCacheImage(`${rootUrl}/img/icon/telecom_1.png`);
-    
     const authorAvatar = fm.fileExists(getAvatarImg()) ? await module.toBase64(fm.readImage(getAvatarImg()) ) : await module.getCacheImage(`${rootUrl}/img/icon/4qiao.png`);
     
     const collectionCode = await module.getCacheImage(`${rootUrl}/img/picture/collectionCode.jpeg`);
@@ -311,16 +309,12 @@ async function main() {
      * 当用户点击底部弹窗时，显示/隐藏弹窗动画，并显示预设消息的打字效果。
      */
     const popupHtml = module.buttonPopup({
+      settings,
       formItems,
       avatarInfo,
-      appImage,
       appleHub_dark,
       appleHub_light,
-      id: 'cookie',
-      buttonColor: 'jb-green',
-      margin: '35px;',
-      text: '中国电信天翼',
-      text2: '实时账单'
+      toggle: true
     });
     
     /**
@@ -351,7 +345,7 @@ async function main() {
         ${await popupHtml}
         <section id="settings">
         </section>
-        <script>${await module.runScripts(formItems, settings, 'cookie', 'range-separ2')}</script>
+        <script>${await module.runScripts(formItems, settings, 'install', 'range-separ2')}</script>
         ${scriptTags}
       </body>
     </html>`;
