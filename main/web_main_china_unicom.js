2cefc6201a9352aa7649833adad8bd3094751d84
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-12-12 17:34:43 +0800

    Update File:  main/web_main_china_unicom.js

--- main/web_main_china_unicom.js
+++ main/web_main_china_unicom.js
@@ -111,23 +111,9 @@ async function main() {
     }
   };
   
-  /**
-   * 获取背景图片存储目录路径
-   * @returns {string} - 目录路径
-   */
-  const getBgImage = (image) => {
-    const filePath =  fm.joinPath(cacheImg, Script.name());
-    if (image) fm.writeImage(filePath, image);
-    return filePath;
+  const ScriptableRun = () => {
+    Safari.open('scriptable:///run/' + encodeURIComponent(Script.name()));
   };
-  
-  // 获取头像图片
-  const getAvatarImg = () => {
-    return fm.joinPath(cacheImg, 'userSetAvatar.png');
-  };
-  
-  // ScriptableRun
-  const ScriptableRun = () => Safari.open('scriptable:///run/' + encodeURIComponent(Script.name()));
   
   // 预览组件，获取版本名称和链接
   const previewWidget = async () => {
@@ -197,12 +183,21 @@ async function main() {
     await module.appleOS_update();
     
     const hours = Math.floor((Date.now() - settings.updateTime) % (24 * 3600 * 1000) / (3600 * 1000));
-    
     if (version !== settings.version && hours >= 12) {
       settings.updateTime = Date.now();
       writeSettings(settings);
       module.notify(`${scriptName}‼️`, `新版本更新 Version ${version}，修复已知问题及布局调整`, 'scriptable:///run/' + encodeURIComponent(Script.name()));
     }
+  };
+  
+  /**
+   * 获取背景图片存储目录路径
+   * @returns {string} - 目录路径
+   */
+  const getBgImage = (image) => {
+    const filePath =  fm.joinPath(cacheImg, Script.name());
+    if (image) fm.writeImage(filePath, image);
+    return filePath;
   };
   
   // ====== web start ======= //
@@ -213,12 +208,17 @@ async function main() {
       previewImage
     } = options;
     
+    const avatarImage = fm.joinPath(cacheImg, 'userSetAvatar.png');
+    const authorAvatar = await module.toBase64(
+    fm.fileExists(avatarImage) 
+      ? fm.readImage(avatarImage) 
+      : await module.getCacheImage(`${rootUrl}/img/icon/4qiao.png`)
+    );
+    
     const appleHub_light = await module.getCacheImage(`${rootUrl}/img/picture/appleHub_white.png`);
     const appleHub_dark = await module.getCacheImage(`${rootUrl}/img/picture/appleHub_black.png`);
     
     const appImage = await module.getCacheImage(`${rootUrl}/img/icon/china_unicom.png`);
-    
-    const authorAvatar = fm.fileExists(getAvatarImg()) ? await module.toBase64(fm.readImage(getAvatarImg()) ) : await module.getCacheImage(`${rootUrl}/img/icon/4qiao.png`);
     
     const collectionCode = await module.getCacheImage(`${rootUrl}/img/picture/collectionCode.jpeg`);
     
@@ -514,9 +514,9 @@ async function main() {
       // switch
       switch (code) {
         case 'setAvatar':
-          const avatarImage = Image.fromData(Data.fromBase64String(data));
           fm.writeImage(
-            getAvatarImg(), await module.drawSquare(avatarImage)
+            avatarImage, 
+            await module.drawSquare( Image.fromData(Data.fromBase64String(data)) )
           );
           break;
         case 'telegram':
