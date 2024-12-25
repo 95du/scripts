0222ec3e615d576144d57ac28d67c16eae456d3b
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-12-26 04:13:33 +0800

    update api/web_sports_rank.js

--- api/web_sports_rank.js
+++ api/web_sports_rank.js
@@ -119,11 +119,11 @@ async function main(family) {
       widget.backgroundGradient = gradient;
     } else {
       widget.backgroundColor = Color.dynamic(Color.white(), Color.black());
-      if (family === 'medium') {
+      //if (family === 'medium') {
         const random = Math.ceil(Math.random() * 6);
       const backgroundImage = await module.getCacheData(`${rootUrl}/img/background/football-player_${random}.png`);
       widget.backgroundImage = backgroundImage;
-      }
+      
       
       
     }
@@ -144,7 +144,7 @@ async function main(family) {
     widget.setPadding(15, 18, 15, 18);
     widget.url = url;
     await setBackground(widget);
-    const maxCol = family === 'medium' ? 6 : data.matchData.length >= 15 ? 15 : data.matchData.length
+    const maxCol = family === 'medium' ? 6 : data.matchData.length >= 14 ? 14 : data.matchData.length
     for (let i = 0; i < maxCol; i++) {
       const team = data.matchData[i];
       const teamStack = widget.addStack();
