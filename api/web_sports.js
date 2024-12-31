b26365efccff189133bb5f75e8940cfea16e09f9
95du丶茅台 <114467794+95du@users.noreply.github.com>
2024-12-31 17:40:05 +0800

    Update File:  api/web_sports.js

--- api/web_sports.js
+++ api/web_sports.js
@@ -70,25 +70,7 @@ async function main(family) {
       const shadowImg = fm.readImage(bgImage);
       widget.backgroundImage = await module.shadowImage(shadowImg);
     } else if (setting.gradient.length > 0 && !setting.bwTheme) {
-      const gradient = new LinearGradient();
-      const color = setting.gradient.length > 0 
-        ? setting.gradient 
-        : [setting.rangeColor];
-      const randomColor = module.getRandomItem(color);
-      // 渐变角度
-      const angle = setting.angle;
-      const radianAngle = ((360 - angle) % 360) * (Math.PI / 180);
-      const x = 0.5 + 0.5 * Math.cos(radianAngle);
-      const y = 0.5 + 0.5 * Math.sin(radianAngle);
-      gradient.startPoint = new Point(1 - x, y);
-      gradient.endPoint = new Point(x, 1 - y);
-      
-      gradient.locations = [0, 1];
-      gradient.colors = [
-        new Color(randomColor, setting.transparency),
-        new Color('#00000000')
-      ];
-      widget.backgroundGradient = gradient;
+      widget.backgroundGradient = module.createGradient();
     } else {
       widget.backgroundColor = Color.dynamic(Color.white(), Color.black());
     }
