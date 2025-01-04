3a5613ff53f5ba0a250885801251df8f2a16fe64
95du丶茅台 <114467794+95du@users.noreply.github.com>
2025-01-04 22:18:37 +0800

    update api/web_sports.js

--- api/web_sports.js
+++ api/web_sports.js
@@ -7,7 +7,7 @@
  * 组件版本: Version 1.0.0
  * 发布时间: 2025-01-01
  */
-await main(config.widgetFamily || 'medium')
+
 async function main(family) {
   const fm = FileManager.local();
   const depPath = fm.joinPath(fm.documentsDirectory(), '95du_module');
