68200890a365ebcd4bba87e6f73157838b83610c
95du丶茅台 <114467794+95du@users.noreply.github.com>
2025-01-08 00:13:18 +0800

    Update File:  main/web_main_12123.js

--- main/web_main_12123.js
+++ main/web_main_12123.js
@@ -671,8 +671,7 @@ async function main() {
           icon: {
             name: 'folder.fill',
             color: '#B07DFF'
-          },
-          desc: 'Honye'
+          }
         },
         {
           label: '刷新时间',
