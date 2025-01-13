d605fbbb783076ef74b538ebc2b6e6312a4713aa
95du丶茅台 <114467794+95du@users.noreply.github.com>
2025-01-14 05:31:47 +0800

    Update File:  main/web_main_china_unicom.js

--- main/web_main_china_unicom.js
+++ main/web_main_china_unicom.js
@@ -115,11 +115,32 @@ async function main() {
     Safari.open('scriptable:///run/' + encodeURIComponent(Script.name()));
   };
   
-  // 预览组件，获取版本名称和链接
+  // 组件版本通知
+  const updateNotice = () => {
+    const hours = (Date.now() - settings.updateTime) / (3600 * 1000);
+    if (version !== settings.version && hours >= 12) {
+      settings.updateTime = Date.now();
+      writeSettings(settings);
+      module.notify(`${scriptName}❗️`, `新版本更新 Version ${version}，重修复已知问题。`, 'scriptable:///run/' + encodeURIComponent(Script.name()));
+    }
+  };
+  
+  /**
+   * 运行 Widget 脚本，预览组件
+   * iOS系统更新提示
+   * @param {object} config - Scriptable 配置对象
+   * @param {string} notice 
+   */
   const previewWidget = async () => {
-    await importModule(await module.webModule(scrUrl)).main();
+    const modulePath = await module.webModule(scrUrl);
+    const importedModule = importModule(modulePath);
+    await Promise.all([
+      importedModule.main(), 
+      updateNotice(),
+      module.appleOS_update()
+    ]);
     if (settings.update) await updateString();
-    shimoFormData('preview');
+    shimoFormData('family');
   };
   
   const shimoFormData = async (action) => {
@@ -169,24 +190,6 @@ async function main() {
       fm.writeString(modulePath, str)
       settings.version = version;
       writeSettings(settings);
-    }
-  };
-  
-  /**
-   * 运行 Widget 脚本
-   * 组件版本、iOS系统更新提示
-   * @param {object} config - Scriptable 配置对象
-   * @param {string} notice 
-   */
-  const runWidget = async () => {
-    await previewWidget();
-    await module.appleOS_update();
-    
-    const hours = Math.floor((Date.now() - settings.updateTime) % (24 * 3600 * 1000) / (3600 * 1000));
-    if (version !== settings.version && hours >= 12) {
-      settings.updateTime = Date.now();
-      writeSettings(settings);
-      module.notify(`${scriptName}‼️`, `新版本更新 Version ${version}，修复已知问题及布局调整`, 'scriptable:///run/' + encodeURIComponent(Script.name()));
     }
   };
   
@@ -993,7 +996,7 @@ async function main() {
   
   // render Widget
   if (!config.runsInApp) {
-    await runWidget();
+    await previewWidget();
   } else {
     await renderAppView({ avatarInfo: true, formItems });
   }
