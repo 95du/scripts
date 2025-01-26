8023b6a9e6a3f7b35e4c76669303c5168508dafb
95du丶茅台 <114467794+95du@users.noreply.github.com>
2025-01-27 01:58:39 +0800

    Update File:  api/web_sports.js

--- api/web_sports.js
+++ api/web_sports.js
@@ -522,6 +522,13 @@ async function main(family) {
           });
         }
         
+        // 检查是否即将开赛小于等于 1 小时
+        const startTime = new Date(match.startTime || match.startTimeStamp * 1000);
+        const startTimeDiff = (startTime - new Date()) / (60 * 1000);
+        if (startTimeDiff > -180 && startTimeDiff <= 60 && !showTitle) {
+          updateCacheFile();
+        }
+        
         const stack = widget.addStack();
         stack.size = new Size(0, lay.stackSize);
         stack.url = raceScheduleUrl;
@@ -917,12 +924,6 @@ async function main(family) {
       const isMediumSwitch = family === 'medium' && setting.autoSwitch;
       const isLargeSwitch = family === 'large' && setting.largeSwitch;
       if (matches && isFootballOrBasketball && (isMediumSwitch || isLargeSwitch)) {
-        // 检查是否即将开赛小于等于 1 小时
-        const startTime = new Date(matches.startTime || matches.startTimeStamp * 1000);
-        const startTimeDiff = (startTime - new Date()) / (60 * 1000);
-        if (startTimeDiff > -180 && startTimeDiff <= 60) {
-          updateCacheFile();
-        }
         widget = await createLiveWidget(matches);
       }
     }
