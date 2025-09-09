71b52107d597f942f05ce32d4787adf336d4906b
95du丶茅台 <114467794+95du@users.noreply.github.com>
2025-09-10 01:45:21 +0800

    Update File:  update/adjustmentDate.js

--- update/adjustmentDate.js
+++ update/adjustmentDate.js
@@ -1,5 +1,8 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-green; icon-glyph: magic;
   <meta charset="utf-8" >
-    <title>全国油价调整日期</title>
+    <title>全国油价调整日期2</title>
   <meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
   <style type="text/css">
    .box{
