fcc0126171d696e9071b81a77fca5c95c8288226
95du丶茅台 <114467794+95du@users.noreply.github.com>
2026-07-14 13:45:43 +0800

    Update File:  api/web_sports.js

--- api/web_sports.js
+++ api/web_sports.js
@@ -915,14 +915,14 @@ async function main(family) {
     await createTopStack(widget, matchId, pageUrl);
     if (family === 'large') {
       widget.addSpacer();
-      if (stat?.list.length && matchStatus !== '0' && setting.events && !setting.statistics) {
+      if (stat?.list.length && matchStatus !== '0' && setting.events && !setting.statistics && matchType === 'football') {
         const { left, right } = parseStats(stat);
         matchEventsColumnStack(widget, left, right);
       } 
       if (stat?.list.length >= 10 && setting.statistics) {
         createStatisticsWidget(widget, stat.list, matchType, matchId);
       } else {
-        await createMatches(widget, (setting.events && stat && matchStatus !== '0' ? 7 : 8));
+        await createMatches(widget, (matchType === 'football' && setting.events && stat && matchStatus !== '0' ? 7 : 8));
       }
     };
     return widget;
