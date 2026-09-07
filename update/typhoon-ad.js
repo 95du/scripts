f3f1043187d4edc125bf51ed4d85d726e20fb909
95du丶茅台 <114467794+95du@users.noreply.github.com>
2026-09-07 15:31:08 +0800

    Update File:  update/typhoon-ad.js

---
+++ update/typhoon-ad.js
@@ -0,0 +1,28 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-green; icon-glyph: magic;
+let body = $response.body;
+try {
+  let obj = JSON.parse(body);
+  if (obj && obj.data && Array.isArray(obj.data)) {
+    obj.data.forEach(item => {
+      // 首页广告
+      if (item.code === "TYPHOON_HOME_AD") {
+        item.appShow = false;
+        if (item.data && item.data.common) {
+          item.data.common.list = [];
+          item.data.common.delay = 0;
+        }
+      }
+      // 详情页广告
+      if (item.code === "TYPHOON_HOME_DETAIL_AD") {
+        item.appShow = false;
+        if (item.data && item.data.common) {
+          item.data.common.list = [];
+        }
+      }
+    });
+  }
+  body = JSON.stringify(obj);
+} catch (e) {}
+$done({ body });
